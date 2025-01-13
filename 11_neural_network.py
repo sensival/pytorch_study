596759978e4d9b57db8fb61c7cd22a20c0d401ca
sensival <alstjs883@naver.com>
2025-01-13 22:34:07 +0900

    nn.Conv2d

--- 11_neural_network.py
+++ 11_neural_network.py
@@ -27,3 +27,15 @@ print(output.size())
 # nn.Linear 계층은 학습 가능한 가중치와 편향을 포함함.
 
 # nn.Conv2d
+
+input = torch.randn(20, 16, 50, 100)
+print(input.size())
+m = nn.Conv2d(16, 33, 3, stride=2)
+m = nn.Conv2d(16, 33, (3, 5), stride=(2, 1), padding=(4, 2))
+m = nn.Conv2d(16, 33, (3, 5), stride=(2, 1), padding=(4, 2), dilation=(3, 1))
+print(m)
+
+# dilation은 커널 내부의 요소 간격을 조절하는 옵션입니다. 기본적으로 dilation=1이면 커널의 요소들이 연속된 픽셀을 대상으로 계산합니다. 하지만 dilation > 1이면 커널 내부에서 요소들이 일정한 간격을 띄고 샘플링하게 됩니다.
+# dilation=(1,1) (기본값)	(3,5)
+# dilation=(2,1)	(5,5)
+# dilation=(3,1)	(7,5)
