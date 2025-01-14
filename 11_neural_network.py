43801de5721ddd9cd53755f2b8067e8d5160efa5
sensival <alstjs883@naver.com>
2025-01-14 22:37:58 +0900

    nn.Conv2d

--- 11_neural_network.py
+++ 11_neural_network.py
@@ -36,6 +36,10 @@ m = nn.Conv2d(16, 33, (3, 5), stride=(2, 1), padding=(4, 2), dilation=(3, 1))
 print(m)
 
 # dilation은 커널 내부의 요소 간격을 조절하는 옵션입니다. 기본적으로 dilation=1이면 커널의 요소들이 연속된 픽셀을 대상으로 계산합니다. 하지만 dilation > 1이면 커널 내부에서 요소들이 일정한 간격을 띄고 샘플링하게 됩니다.
+# 설정	커널이 커버하는 실제 크기
 # dilation=(1,1) (기본값)	(3,5)
 # dilation=(2,1)	(5,5)
 # dilation=(3,1)	(7,5)
+
+output = m(input)
+print(output.size())
